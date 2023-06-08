da814794868eb2e40f1148dc7c6033790a89130e
sushashi <shuishan.ly@gmail.com>
2023-06-08 16:14:40 +0200

    part 4 submission

---
+++ part4/blog/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+
+const mongoUrl = config.MONGODB_URL
+
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.tokenExtractor)
+
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
