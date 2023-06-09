55cff48c7f59f859f796ab33f89fd9c586432cdb
sushashi <shuishan.ly@gmail.com>
2023-06-09 11:56:01 +0200

    router modification for authentification

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const userExtractor = require('../utils/middleware').userExtractor
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user',{ username: 1, name: 1})
@@ -12,7 +13,7 @@ blogsRouter.get('/:id', async (request, response) => {
     response.status(200).json(blog)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', userExtractor, async (request, response) => {
     const body = request.body
     const userT = request.user
     const user = await User.findById(userT.id)
@@ -32,7 +33,7 @@ blogsRouter.post('/', async (request, response) => {
     response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
     const user = request.user
     const blog = await Blog.findById(request.params.id)
 
@@ -45,7 +46,7 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 })
 
-blogsRouter.put('/:id', async (request, response) => {
+blogsRouter.put('/:id', userExtractor, async (request, response) => {
     const body = request.body
     const blog = {
         author: body.author,
