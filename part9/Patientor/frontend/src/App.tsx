1bea122531321a2c6d91e2c24ba086af9e3c2131
sushashi <shuishan.ly@gmail.com>
2023-10-12 17:03:53 +0200

    File rename

--- part9/Patientor/frontend/src/App.tsx
+++ part9/Patientor/frontend/src/App.tsx
@@ -8,7 +8,7 @@ import { Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
-import PatientSingle from "./components/PatientSingle/PatientSingle";
+import PatientSingle from "./components/PatientSingle";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
